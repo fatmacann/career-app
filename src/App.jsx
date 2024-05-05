f62874095d06a9fff50a7a4a91a9eba36c06d9db
fatmacann <canf261012@gmail.com>
2024-05-05 23:11:52 +0300

    start home page

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
+import HomePage from './pages/HomePage';
+
+import NotFoundPage from './pages/NotFoundPage';
+
+const App = () => {
+  // Add New Job
+ 
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+
+
+        <Route path='*' element={<NotFoundPage />} />
+      </Route>
+    )
+  );
+
+  return <RouterProvider router={router} />;
+};
+export default App;
