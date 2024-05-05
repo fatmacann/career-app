a2f06e32051e0f3425dc800c63b03753137715b3
fatmacann <canf261012@gmail.com>
2024-05-06 00:13:56 +0300

    job list

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,15 @@ import {
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 
-
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+    </Route>
+  )
+);
 
 const App = () => {
-  // Add New Job
- 
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path='/' element={<MainLayout />}>
-        <Route index element={<HomePage />} />
-
-
-      </Route>
-    )
-  );
-
   return <RouterProvider router={router} />;
 };
 export default App;
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,6 +1,4 @@
 import { Outlet } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import Navbar from '../components/Navbar';
 
 const MainLayout = () => {
@@ -8,7 +6,6 @@ const MainLayout = () => {
     <>
       <Navbar />
       <Outlet />
-      <ToastContainer />
     </>
   );
 };
